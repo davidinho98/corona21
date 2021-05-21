12946a7ebb55438e9c28c53fb8bda3a9a78146ca
davidinho98 <s1810456013@students.fh-hagenberg.at>
2021-05-21 23:50:46 +0200

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { VaccinationListComponent } from './vaccination-list/vaccination-list.component';
@@ -11,11 +11,12 @@ import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchComponent } from './search/search.component';
+import { VaccinationFormComponent } from './vaccination-form/vaccination-form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule ],
-  declarations: [ AppComponent, VaccinationListComponent, VaccinationListItemComponent, VaccinationDetailsComponent, HomeComponent, SearchComponent ],
-  bootstrap:    [ AppComponent ],
+  imports:[ BrowserModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule ],
+  declarations: [ AppComponent, VaccinationListComponent, VaccinationListItemComponent, VaccinationDetailsComponent, HomeComponent, SearchComponent, VaccinationFormComponent ],
+  bootstrap: [ AppComponent ],
   providers: [VaccinationService]
 })
 export class AppModule { }
---
+++ src/app/vaccination-form/vaccination-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vaccination-form',
+  templateUrl: './vaccination-form.component.html'
+})
+export class VaccinationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

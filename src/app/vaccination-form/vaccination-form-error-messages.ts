263a6aceca853377641f0dc218f641bedadaab7c
davidinho98 <s1810456013@students.fh-hagenberg.at>
2021-05-22 00:31:38 +0200

    1 file modified, 2 added

---
+++ src/app/vaccination-form/vaccination-form-error-messages.ts
@@ -0,0 +1,27 @@
+export class ErrorMessage {
+  constructor(
+    public forControl: string,
+    public forValidator: string,
+    public text: string
+  ) {}
+}
+export const VaccinationFormErrorMessages = [
+  new ErrorMessage(
+    'amount', 'required', 'Bitte geben Sie den Wert für die maximale TeilnehmerInnenanzahl an.'
+  ),
+  new ErrorMessage(
+    'amount', 'min', 'Achten Sie bei der Eingabe bitte auf eine positive Mindestanzahl.'
+  ),
+  new ErrorMessage(
+    'date', 'required', 'Wählen Sie bitte ein Datum für die Impfung aus.'),
+
+  new ErrorMessage(
+    'start', 'required', 'Bitte Startzeit der Impfung angeben.'),
+
+  new ErrorMessage(
+    'end', 'required', 'Bitte Endzeit der Impfung angeben.'),
+
+  /*new ErrorMessage(
+    'location', 'required', 'Bitte einen Ort für die Impfung auswählen.')*/
+
+];
--- src/app/vaccination-form/vaccination-form.component.ts
+++ src/app/vaccination-form/vaccination-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Vaccination } from '../shared/location';
+import { VaccinationFactory } from '../shared/vaccination-factory';
+import { VaccinationService } from '../shared/vaccination.service';
+import { VaccinationFormErrorMessages } from './vaccination-form-error-messages';
 
 @Component({
   selector: 'app-vaccination-form',
@@ -6,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VaccinationFormComponent implements OnInit {
 
-  constructor() { }
+  vaccinationForm: FormGroup;
+  vaccination = VaccinationFactory.empty();
+  isUpdatingVaccination = false;
+  errors:{[key:string]:string}={};
+
+  constructor(private fb:FormBuilder, private bs:VaccinationService, private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params["id"];
+    if(id){
+      this.isUpdatingVaccination = true;
+      this.bs.getSingle(id).subscribe(vaccination =>{
+        this.vaccination = vaccination;
+        this.initVaccination();
+      });
+    }
+    this.initVaccination();
+  }
+
+  initVaccination(){
+      //Formular bauen
+      this.vaccinationForm = this.fb.group({
+        amount: [this.vaccination.amount,Validators.required] 
+      });
+  }
+
+  updateErrorMessages(){
+    this.errors = {};
+    for (const message of VaccinationFormErrorMessages){
+      const control = this.vaccinationForm.get(message.forControl);
+      if(control && control.dirty && control.invalid && control.errors[message.forValidator] && !this.errors[message.forControl]){
+        
+      }
+    }
   }
 
 }

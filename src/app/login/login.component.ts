a3290790be970e3a066f19831315a979adef7b18
davidinho98 <s1810456013@students.fh-hagenberg.at>
2021-05-23 01:12:23 +0200

    1 file modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../shared/authentication.service';
 
 @Component({
-  selector: 'app-login',
+  selector: 'bs-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private fb:FormBuilder, private router:Router, private authService:AuthenticationService) { }
+  constructor(private fb:FormBuilder, private router:Router, private authService: AuthenticationService) { }
 
   ngOnInit() {
   }

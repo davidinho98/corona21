71e28350f8a826bb263eb06bd9f2ccff774dbfb6
davidinho98 <s1810456013@students.fh-hagenberg.at>
2021-05-23 02:27:05 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CanNavigateToAdminGuard } from './shared/can-navigate-to-admin.guard';
 import { VaccinationDetailsComponent } from './vaccination-details/vaccination-details.component';
 import { VaccinationFormComponent } from './vaccination-form/vaccination-form.component';
 import { VaccinationListComponent } from './vaccination-list/vaccination-list.component';
@@ -11,13 +12,14 @@ const routes: Routes = [
   {path:'home', component: HomeComponent},
   {path:'vaccinations', component:VaccinationListComponent},
   {path:'vaccinations/:id', component:VaccinationDetailsComponent},
-  {path:'admin', component:VaccinationFormComponent},
+  {path:'admin', component:VaccinationFormComponent, canActivate:[CanNavigateToAdminGuard]},
   {path:'admin/:id', component:VaccinationFormComponent},
   {path:'login', component:LoginComponent}
 ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [CanNavigateToAdminGuard]
 })
 export class AppRoutingModule { }

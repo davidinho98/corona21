79cf0823e954bbcae9807a9370303a758de109da
davidinho98 <s1810456013@students.fh-hagenberg.at>
2021-05-24 03:43:25 +0200

    4 files modified, 1 added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,8 @@ import { Vaccination, Location } from '../shared/location';
 
 @Component({
   selector: 'bs-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent implements OnInit {
 
